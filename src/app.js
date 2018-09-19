a570c728ced4d3c8582ef0816df84fea809f6372
MarvinJWendt <github@marvinjwendt.com>
2018-09-19 16:20:14 +0200

    ~ reformat

--- src/app.js
+++ src/app.js
@@ -7,14 +7,10 @@ module.exports = function (hash, string) {
 
 module.exports.md5 = function (hash, string) {
   let hashed = crypto.createHash('md5').update(string).digest('hex')
-  if (hash.toString() === hashed.toString()) return true
-
-  return false
+  return (hash.toString() === hashed.toString())
 }
 
 module.exports.sha1 = function (hash, string) {
   let hashed = crypto.createHash('sha1').update(string).digest('hex')
-  if (hash.toString() === hashed.toString()) return true
-
-  return false
+  return (hash.toString() === hashed.toString())
 }
