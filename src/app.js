dfd1a1a4fb5e3a03b4618b8ce74efbf7264f497b
MarvinJWendt <github@marvinjwendt.com>
2018-09-19 16:18:49 +0200

    - console output while hashing

--- src/app.js
+++ src/app.js
@@ -7,7 +7,6 @@ module.exports = function (hash, string) {
 
 module.exports.md5 = function (hash, string) {
   let hashed = crypto.createHash('md5').update(string).digest('hex')
-  console.log(hashed.toString())
   if (hash.toString() === hashed.toString()) return true
 
   return false
@@ -15,7 +14,6 @@ module.exports.md5 = function (hash, string) {
 
 module.exports.sha1 = function (hash, string) {
   let hashed = crypto.createHash('sha1').update(string).digest('hex')
-  console.log(hashed.toString())
   if (hash.toString() === hashed.toString()) return true
 
   return false
