a4aba261341ea8b8e925b70aa585a01ae25848bb
MarvinJWendt <github@marvinjwendt.com>
2018-09-19 17:02:23 +0200

    + tests

--- test/test.js
+++ test/test.js
@@ -15,13 +15,25 @@ test('Crypto supported', t => {
 })
 
 test('Check MD5', t => {
-  t.is(app.md5('b10a8db164e0754105b7a99be72e3fe5', 'Hello World'), true)
+  t.is(app('b10a8db164e0754105b7a99be72e3fe5', 'Hello World'), true)
 })
 
 test('Check SHA1', t => {
-  t.is(app.sha1('0a4d55a8d778e5022fab701977c5d840bbc486d0', 'Hello World'), true)
+  t.is(app('0a4d55a8d778e5022fab701977c5d840bbc486d0', 'Hello World'), true)
 })
 
-test('All', t => {
-  t.is(app('0a4d55a8d778e5022fab701977c5d840bbc486d0', 'Hello World'), true)
+test('Check SHA256', t => {
+  t.is(app('a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b277d9ad9f146e', 'Hello World'), true)
+})
+
+test('Check SHA512', t => {
+  t.is(app('2c74fd17edafd80e8447b0d46741ee243b7eb74dd2149a0ab1b9246fb30382f27e853d8585719e0e67cbda0daa8f51671064615d645ae27acb15bfb1447f459b', 'Hello World'), true)
+})
+
+test('Check RIPEMD-160', t => {
+  t.is(app('a830d7beb04eb7549ce990fb7dc962e499a27230', 'Hello World'), true)
+})
+
+test('Check Whirlpool', t => {
+  t.is(app('b77b284bffc952efee36a94397a0ce11e8624668e33b7020a80eb2fb21096f0a08518c50d023de12b010c2e30b93b5837dc471d899608d786fe9a6b60112ea4a', 'Hello World'), true)
 })
