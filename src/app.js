2daa4d172cfd937d36697eacc92a82d9a4600cd3
MarvinJWendt <github@marvinjwendt.com>
2018-09-19 16:15:37 +0200

    Added MD5 support

--- src/app.js
+++ src/app.js
@@ -1,3 +1,14 @@
-exports.main = function () {
-  return "ABC"
+const crypto = require('crypto')
+
+
+module.exports = function (hash, string) {
+
+}
+
+module.exports.md5 = function (hash, string) {
+  let hashed = crypto.createHash('md5').update(string).digest('hex')
+  console.log(hashed.toString())
+  if (hash.toString() === hashed.toString()) return true
+
+  return false
 }
--- test/test.js
+++ test/test.js
@@ -1,6 +1,22 @@
 import test from 'ava'
-const app = require("../src/app")
 
-test('Sample test', t => {
-  t.is(app.main(), "ABC")
+const app = require('../src/app')
+
+test('Crypto supported', t => {
+  try {
+    let crypto = require('crypto')
+  }
+  catch (err) {
+    t.fail('Crypto is disabled!')
+  }
+
+  t.pass()
+})
+
+test('Check MD5', t => {
+  t.is(app.md5('b10a8db164e0754105b7a99be72e3fe5', 'Hello World'), true)
+})
+
+test('Check SHA1', t => {
+  t.is(app.sha1('0a4d55a8d778e5022fab701977c5d840bbc486d0', 'Hello World'), true)
 })
