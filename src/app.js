3350cd927732a78873adc19ad53cf3e5872b53fe
MarvinJWendt <github@marvinjwendt.com>
2018-09-19 19:25:26 +0200

    ~ cleanup

--- src/app.js
+++ src/app.js
@@ -1,13 +1,9 @@
 const crypto = require('crypto')
 
 module.exports = function (hash, string) {
-  if (string === null) {
-    throw TypeError('String cannot be null')
-  }
 
-  if (hash === null) {
-    throw TypeError('Hash cannot be null')
-  }
+  if (string === null) throw TypeError('String cannot be null')
+  if (hash === null) throw TypeError('Hash cannot be null')
 
   for (let algo in crypto.getHashes()) {
     try {
