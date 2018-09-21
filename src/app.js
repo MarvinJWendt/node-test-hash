3d5de804ff3b82b5809d7b51d9bbf62df4a412f3
MarvinJWendt <github@marvinjwendt.com>
2018-09-21 15:07:50 +0200

    ~ updated to typescript

--- src/app.js
+++ src/app.js
@@ -1,25 +1,27 @@
-const crypto = require('crypto')
-
+"use strict";
+const crypt = require('crypto');
 module.exports = function (hash, string, algorithm = 'guess') {
-
-  console.log(algorithm)
-
-  if (string === null) throw TypeError('String cannot be null')
-  if (hash === null) throw TypeError('Hash cannot be null')
-
-  if (algorithm === 'guess') {
-    for (let algo in crypto.getHashes()) {
-      try {
-        if (crypto.createHash(crypto.getHashes()[algo]).update(string).digest('hex').toString() === hash.toString()) return true
-      } catch (err) {
-        console.log(err)
-      }
+    console.log(algorithm);
+    if (string === null)
+        throw TypeError('String cannot be null');
+    if (hash === null)
+        throw TypeError('Hash cannot be null');
+    if (algorithm === 'guess') {
+        for (let index in crypt.getHashes()) {
+            try {
+                if (crypt.createHash(crypt.getHashes()[index]).update(string).digest('hex').toString() === hash.toString())
+                    return true;
+            }
+            catch (err) {
+                console.log(err);
+            }
+        }
     }
-  } else {
-    if (algorithm == null) throw TypeError('Algorithm cannot be null')
-    if (crypto.createHash(algorithm).update(string).digest('hex').toString() === hash.toString()) return true
-  }
-
-  return false
-}
-
+    else {
+        if (algorithm == null)
+            throw TypeError('Algorithm cannot be null');
+        if (crypt.createHash(algorithm).update(string).digest('hex').toString() === hash.toString())
+            return true;
+    }
+    return false;
+};
---
+++ src/app.ts
@@ -0,0 +1,25 @@
+const crypt = require('crypto')
+
+module.exports = function (hash: string, string: string, algorithm = 'guess') {
+
+  console.log(algorithm)
+
+  if (string === null) throw TypeError('String cannot be null')
+  if (hash === null) throw TypeError('Hash cannot be null')
+
+  if (algorithm === 'guess') {
+    for (let index in crypt.getHashes()) {
+      try {
+        if (crypt.createHash(crypt.getHashes()[index]).update(string).digest('hex').toString() === hash.toString()) return true
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  } else {
+    if (algorithm == null) throw TypeError('Algorithm cannot be null')
+    if (crypt.createHash(algorithm).update(string).digest('hex').toString() === hash.toString()) return true
+  }
+
+  return false
+}
+
